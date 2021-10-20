6e622c6b758d91f6f0db1498eb53b9429164e64a
Guilherme Teixeira <24235344+vibraniumdev@users.noreply.github.com>
2021-10-19 22:00:46 -0300

    Removed all directories and its contents

--- 01-MentalModels/mentalModels.js
+++
@@ -1,6 +0,0 @@
-let a = 10;
-let b = a; 
-a = 0;
-
-console.log("A variável 'a' vale:", a) 
-console.log("A variável 'b' vale:", b)
--- 03-values-and-variables/valuesAndVariables.js
+++
@@ -1,6 +0,0 @@
-// let reaction = 'yikes'
-// reaction[0] = 'l'
-// console.log(reaction)
-
-let arr = [212, 8, 506];
-let str = "hello";
